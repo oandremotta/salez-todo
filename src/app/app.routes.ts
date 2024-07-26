017cad2a6423b8b550b2ae0ff1a7684cdced466f
Andre Motta <andre.motta@multfacilcomercial.com.br>
2024-07-26 02:04:43 -0300

    Update route redirections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,8 @@ export const routes: Routes = [
     loadChildren: () => import('./pages/tabs/tabs.routes').then(m => m.routes),
   },
   {
-    path: 'users',
-    loadChildren: () => import('./pages/users/users.routes').then(m => m.routes),
-  },
-  {
-    path: 'tasks',
-    loadComponent: () => import('./pages/tasks/tasks.page').then( m => m.TasksPage)
+    path: "",
+    redirectTo: "tabs/tasks",
+    pathMatch: "full"
   }
 ];
--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -20,7 +20,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/users',
+    redirectTo: '/tabs/tasks',
     pathMatch: 'full',
   },
 ];
